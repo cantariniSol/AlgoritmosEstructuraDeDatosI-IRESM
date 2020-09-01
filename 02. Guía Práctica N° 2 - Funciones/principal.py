96741ce5010d04142d22d9e42a0c1be9f598134d
s-passalacqua-iresm <69656420+s-passalacqua-iresm@users.noreply.github.com>
2020-08-31 22:01:00 -0300

    Update principal.py

--- 02. Guía Práctica N° 2 - Funciones/principal.py
+++ 02. Guía Práctica N° 2 - Funciones/principal.py
@@ -11,22 +11,22 @@ def menu():
         num = int(input(">>Ingrese la opción que desea realizar: "))
 
         if (num == 1):
-            s = funciones.mayor10()
-            return print(str(s))
+            s = funciones.mayor10() # La funcion mayor10() no retorna ningun valor por lo que no debería ser guardada en la variable s
+            return print(str(s)) # Esta linea no debe escribirse
         elif (num == 2):
             n1 = int(input("Ingrese un número: "))
             n2 = int(input("Ingrese otro número: "))
             n3 = int(input("Ingrese un último número: "))
-            m = funciones.sumaIgualAlTercero(a=n1, b=n2, c=n3)
-            return print(str(m))
+            m = funciones.sumaIgualAlTercero(a=n1, b=n2, c=n3) # La funcion sumaIgualAlTercero no retorna ningun valor por lo que no debería ser guardada en la variable m
+            return print(str(m)) # Esta linea no debe escribirse
         elif (num == 3):
             n3 = int(input("Ingrese un número: "))
             n4 = int(input("Ingrese otro número: "))
             j = funciones.dividirCuadrado(num1=n3, num2=n4)
-            return print(str(j))
+            return print(str(j)) # La palabra reservada return no debe escribirse, lo que se debería imprimir es print(f"El resultado es: {j}")
         elif (num == 4):
             l = funciones.celcius()
-            return print(str(f"{l}°"))
+            return print(str(f"{l}°")) # La palabra reservada return no debe escribirse
 
 
 
